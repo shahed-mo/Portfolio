0f26b0024bf0a10ac8c94c9fe4bf886e5e8268a8
shahed-mo <fuygug7g7g@gmail.com>
2026-04-19 20:46:37 +0200

    port

--- js/index.js
+++ js/index.js
@@ -6,3 +6,12 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 };
+
+const links = document.querySelectorAll('.navbar a');
+
+links.forEach((link)=>{
+    link.addEventListener('click',()=>{
+        links.forEach(l=>l.classList.remove('active'));
+        link.classList.add('active');
+    })
+})
